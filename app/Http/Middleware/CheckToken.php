473a752b35e8436f9fd07064701057aa23910987
parth developer <parthdeveloper9@gmail.com>
2024-06-27 10:57:23 +0530

    test

--- app/Http/Middleware/CheckToken.php
+++ app/Http/Middleware/CheckToken.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\api_authorization;
 use App\Models\User;
 use Closure;
 use Illuminate\Http\Request;
@@ -20,18 +21,28 @@ class CheckToken
         $sessionToken = $request->token;
         if (!$sessionToken) {
             return response()->json(['error' => 'Unauthorized'], 401);
+        }elseif(!isset($request->site_key) && !isset($request->server_key)){
+            return response()->json(['error' => 'Unauthorized'], 401);
         }
 
-        // Check if the token is present in the database
-        $dbToken = User::where('api_token', $sessionToken)->first();
+        if($sessionToken){
+            // Check if the token is present in the database
+            $dbToken = User::where('api_token', $sessionToken)->first();
 
-        if (!$dbToken) {
-            return response()->json(['error' => 'Invalid token'], 401);
+            if (!$dbToken) {
+                return response()->json(['error' => 'Invalid token'], 401);
+            }
+        }elseif(isset($request->site_key) && isset($request->server_key)){
+            $domainName = $request->getHost();
+            $authorize = api_authorization::where('site_key', $request->site_key)
+                       ->where('server_key', $request->server_key)
+                       ->where('domain_name', 'LIKE', '%' . $domainName . '%')
+                       ->first();
+
+            if (!$authorize) {
+                return response()->json(['error' => 'Unauthorized'], 401);
+            }
         }
-
-        //  if ($request->ajax()) {
-        //     return response()->json(['success' => false, 'message' => 'Unauthorized'], 401);
-        //  }
        
         return $next($request);
     }
