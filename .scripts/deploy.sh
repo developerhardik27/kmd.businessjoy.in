750078425b910ff69a10f1ab23c69ee288c5ec93
parth developer <parthdeveloper9@gmail.com>
2024-07-18 10:49:12 +0530

    try to make auto deploy file dynamic

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -3,17 +3,39 @@ set -e
 
 echo "Deployment started ...."
 
-cd ~/htdocs/staging.businessjoy.in/
+# Define your deployment directories and branches
+declare -A branches=(
+    ["main"]="~/htdocs/businessjoy.in/"
+    ["staging"]="~/htdocs/staging.businessjoy.in/"
+)
+
+# Determine the current branch
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+
+# Check if the current branch exists in your defined branches
+if [[ -n "${branches[$current_branch]}" ]]; then
+    deployment_directory="${branches[$current_branch]}"
+    echo "Deploying branch $current_branch to $deployment_directory"
+
+cd "$deployment_directory"
+# cd ~/htdocs/staging.businessjoy.in/
 
 # Turn ON Maintenance Mode or return true
 # if already is in maintenance mode
 (php artisan down) || true
 
 # Pull the latest version of the app
-git pull origin main
+# git pull origin main
+# Pull the latest version of the app
+git pull origin "$current_branch"
 
 
 # Turn OFF Maintenance mode
 php artisan up
 
 echo "Deployment finished!"
+
+else
+    echo "Error: Deployment for branch $current_branch is not configured."
+    exit 1
+fi
