0173f4c7eec34b0eaf7c4276e06620feb2c3151d
parth developer <parthdeveloper9@gmail.com>
2024-08-05 10:31:51 +0530

    add v1.1.1

---
+++ app/Http/Middleware/v1_1_1/CheckToken.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Middleware\v1_1_1;
+
+use App\Models\api_authorization;
+use App\Models\User;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckToken
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        // Check if the token is present in the session
+        $sessionToken = $request->token;
+        
+        if(!$sessionToken && !isset($request->site_key) && !isset($request->server_key)){
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+        if($sessionToken){
+            // Check if the token is present in the database
+            $dbToken = User::where('api_token', $sessionToken)->first();
+
+            if (!$dbToken) {
+                return response()->json(['error' => 'Invalid token'], 401);
+            }
+        }
+        return $next($request);
+    }
+}
