9c5050e30dd013b28ef0d40318613819b45c4d69
parth developer <parthdeveloper9@gmail.com>
2024-08-05 16:57:04 +0530

    set autocomplete off using jquery

--- resources/views/v1_1_0/admin/userupdateform.blade.php
+++ resources/views/v1_1_0/admin/userupdateform.blade.php
@@ -37,7 +37,7 @@
 @endsection
 
 @section('form-content')
-    <form id="userupdateform" autocomplete="off">
+    <form id="userupdateform" autocomplete="off" list="autocompleteOff">
         @csrf
         <div class="form-group">
             <div class="form-row">
@@ -1650,6 +1650,12 @@
             // response status == 200 that means response succesfully recieved
             // response status == 500 that means database not found
             // response status == 422 that means api has not got valid or required data
+
+
+            $('#userupdateform').attr('autocomplete', 'off');
+            
+            // Set autocomplete="off" for all input fields within the form
+            $('#userupdateform input').attr('autocomplete', 'off');
 
             var userrp = "{{ session('user_permissions.adminmodule.userpermission.edit') }}";
 
