f0e5f8c1e5c6824b6bc604381852e7f925faa645
parth developer <parthdeveloper9@gmail.com>
2024-11-08 17:30:39 +0530

    add recaptcha , add contact form popup , fix responsive issue

--- app/Http/Controllers/landing/LandingPageController.php
+++ app/Http/Controllers/landing/LandingPageController.php
@@ -2,14 +2,15 @@
 
 namespace App\Http\Controllers\landing;
 
-use App\Http\Controllers\Controller;
-use App\Mail\becomePartnerMail;
-use App\Mail\LandingPageMail;
+use GuzzleHttp\Client;
+use App\Models\tbllead;
 use App\Mail\ThankYouMail;
 use App\Models\bj_partner;
-use App\Models\tbllead;
 use Illuminate\Http\Request;
+use App\Mail\LandingPageMail;
+use App\Mail\becomePartnerMail;
 use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Validator;
 
@@ -17,102 +18,145 @@ class LandingPageController extends Controller
 {
     public function new(Request $request)
     {
-
-
+ 
         if (isset($request->subscribe)) {
             $validator = Validator::make($request->all(), [
                 'email' => 'required|email',
             ]);
         } else {
             $validator = Validator::make($request->all(), [
-                'name' => 'required',
+                'name' => 'required|alpha',
                 'email' => 'required|email',
-                'contact_no' => 'required'
-
+                'mobile_number' => 'required|digits_between:10,12',
+                'message' => 'required'
             ]);
         }
 
 
         if ($validator->fails()) {
-            return redirect()->back()->with('error', 'Please enter valid details');
+            return response()->json([
+                'status' => 422,
+                'errors' => $validator->messages()
+            ], 422);
         } else {
 
-            $host = $_SERVER['HTTP_HOST'];
+           
+            // Get the reCAPTCHA response token
+            $recaptchaResponse = $request->input('g-recaptcha-response');
 
-            if ($host === 'localhost:8000') {
-                // If the host is localhost
-                $dbname = 'bj_shree_vinayak_battery_zone_k9r';
-            } elseif ($host === 'staging.businessjoy.in') {
-                // If the host is staging.businessjoy.in
-                $dbname = 'staging_business_joy_parth_fy6';
-            } else {
-                // For any other host, provide a default
-                $dbname = 'business_joy_oceanmnc_fay';
+            if (empty($recaptchaResponse)) {
+                return response()->json([
+                    'status' => 500,
+                    'message' => 'reCAPTCHA response is missing.',
+                ], 500);
             }
+              
+            $secretKey = env('RECAPTCHA_SECRET_KEY'); // Get the secret key from .env file
+
+            // Send a request to Google's reCAPTCHA API to verify the token
+            $client = new Client();
+            $response = $client->post('https://www.google.com/recaptcha/api/siteverify', [
+                'form_params' => [
+                    'secret' => $secretKey,
+                    'response' => $recaptchaResponse,
+                ],
+            ]);
+
+            $data = json_decode($response->getBody()->getContents()); 
+
+            if ($data->success && $data->score >= 0.7) {
+
+                $host = $_SERVER['HTTP_HOST'];
+
+                if ($host === 'localhost:8000') {
+                    // If the host is localhost
+                    $dbname = 'bj_shree_vinayak_battery_zone_k9r';
+                } elseif ($host === 'staging.businessjoy.in') {
+                    // If the host is staging.businessjoy.in
+                    $dbname = 'staging_business_joy_parth_fy6';
+                } else {
+                    // For any other host, provide a default
+                    $dbname = 'business_joy_oceanmnc_fay';
+                }
+
+                config(['database.connections.dynamic_connection.database' => $dbname]);
+
+                // Establish connection to the dynamic database
+                DB::purge('dynamic_connection');
+                DB::reconnect('dynamic_connection');
 
 
 
-            config(['database.connections.dynamic_connection.database' => $dbname]);
+                if (isset($request->subscribe)) {
 
-            // Establish connection to the dynamic database
-            DB::purge('dynamic_connection');
-            DB::reconnect('dynamic_connection');
-
-
-
-            if (isset($request->subscribe)) {
-
-                $checkoldrec = DB::connection('dynamic_connection')->table('tbllead')
-                    ->where('email', $request->email)
-                    ->where('source', 'Business Joy Website Page')
-                    ->get();
+                    $checkoldrec = DB::connection('dynamic_connection')->table('tbllead')
+                        ->where('email', $request->email)
+                        ->where('source', 'Business Joy Website Page')
+                        ->get();
 
 
-                if (count($checkoldrec) > 1) {
+                    if (count($checkoldrec) > 1) { 
+                        return response()->json([
+                            'status' => 500,
+                            'message' => 'You are already subscribed!.',
+                        ], 500); 
+                    }
 
-                    return redirect()->back()->with('error', 'Your are already subscribed!');
+
+                    $data = [
+                        'email' => $request->email,
+                        'audience_type' => 'cool',
+                        'source' => 'Business Joy Website Page'
+                    ];
+                } else {
+
+                    $data = [
+                        'first_name' => $request->name,
+                        'email' => $request->email,
+                        'contact_no' => $request->mobile_number,
+                        'title' => $request->subject,
+                        'msg_from_lead' => $request->message,
+                        'audience_type' => 'cool',
+                        'source' => 'landing page'
+                    ];
                 }
 
 
-                $data = [
-                    'email' => $request->email,
-                    'audience_type' => 'cool',
-                    'source' => 'Business Joy Website Page'
-                ];
-            } else {
+                $lead = DB::connection('dynamic_connection')->table('tbllead')->insert($data);
 
-                $data = [
-                    'first_name' => $request->name,
-                    'email' => $request->email,
-                    'contact_no' => $request->contact_no,
-                    'title' => $request->subject,
-                    'msg_from_lead' => $request->msg,
-                    'audience_type' => 'cool',
-                    'source' => 'landing page'
-                ];
+                if ($lead) {
+
+
+                    $user = 'oceanmnc1512@gmail.com';
+                    Mail::to($request->email)->bcc('parthdeveloper9@gmail.com')->send(new ThankYouMail($request));
+                    Mail::to($user)->bcc('parthdeveloper9@gmail.com')->send(new LandingPageMail($request));
+
+                    return response()->json([
+                        'status' => 200,
+                        'message' => 'Your request successfully submitted.',
+                    ], 200); 
+
+                } else {
+                     return response()->json([
+                        'status' => 500,
+                        'message' => 'Something went wrong! please try again some time later.',
+                    ], 500); 
+                }
+
             }
 
-
-            $lead = DB::connection('dynamic_connection')->table('tbllead')->insert($data);
-
-            if ($lead) {
-
-
-                $user = 'oceanmnc1512@gmail.com';
-                Mail::to($request->email)->bcc('parthdeveloper9@gmail.com')->send(new ThankYouMail($request));
-                Mail::to($user)->bcc('parthdeveloper9@gmail.com')->send(new LandingPageMail($request));
-
-                return redirect()->back()->with('success', 'Your Request succesfully Submited');
-
-            } else {
-                return redirect()->back()->with('error', 'Something Went Wrong!Please try Again Sometime Later');
-            }
+            // If verification fails, send an error response
+            return response()->json([
+                'status' => 500,
+                'message' => 'reCAPTCHA verification failed. Please try again.',
+            ]);
 
 
         }
     }
 
-    public function storeNewPartner(Request $request){
+    public function storeNewPartner(Request $request)
+    {
         $validator = Validator::make($request->all(), [
             'company_name' => 'required|string',
             'company_website' => 'nullable|string',
@@ -128,25 +172,25 @@ class LandingPageController extends Controller
             'contact_person_mobile_number' => 'required|digits_between:10,15',
         ]);
 
-        if($validator->fails()){
+        if ($validator->fails()) {
             return redirect()->back()->withInput()->withErrors($validator);
         }
 
 
-        $checkEmail = bj_partner::where('contact_person_email',$request->contact_person_email)->exists();
+        $checkEmail = bj_partner::where('contact_person_email', $request->contact_person_email)->exists();
         $errors = [];
-        if($checkEmail){
-            $errors['contact_person_email'] = 'You have already requested for partnership. please contact to support.' ;
-        }
-        
-        $checkMobileNumber = bj_partner::where('contact_person_mobile',$request->contact_person_mobile_number)->exists();
-
-        if($checkMobileNumber){
-            $errors['contact_person_mobile_number'] = 'You have already requested for partnership. please contact to support.' ;
+        if ($checkEmail) {
+            $errors['contact_person_email'] = 'You have already requested for partnership. please contact to support.';
         }
 
-        if( count($errors) > 0){
-            Mail::to(env('MAIL_FROM_ADDRESS','businessjoy.in@gmail.com'))->bcc(['parthdeveloper9@gmail.com'])->send(new becomePartnerMail($request));
+        $checkMobileNumber = bj_partner::where('contact_person_mobile', $request->contact_person_mobile_number)->exists();
+
+        if ($checkMobileNumber) {
+            $errors['contact_person_mobile_number'] = 'You have already requested for partnership. please contact to support.';
+        }
+
+        if (count($errors) > 0) {
+            Mail::to(env('MAIL_FROM_ADDRESS', 'businessjoy.in@gmail.com'))->bcc(['parthdeveloper9@gmail.com'])->send(new becomePartnerMail($request));
             return redirect()->back()->withInput()->withErrors($errors);
         }
 
@@ -165,12 +209,12 @@ class LandingPageController extends Controller
             'contact_person_mobile' => $request->contact_person_mobile_number,
         ]);
 
-        if($partner){ 
-            Mail::to($request->contact_person_email)->bcc(['jay@oceanmnc.com','parthdeveloper9@gmail.com'])->send(new becomePartnerMail($request));
+        if ($partner) {
+            Mail::to($request->contact_person_email)->bcc(['jay@oceanmnc.com', 'parthdeveloper9@gmail.com'])->send(new becomePartnerMail($request));
 
-            return redirect()->back()->with('success','Thank You! Your request succesfully submitted');
-        }else{
-            return redirect()->back()->with('error','Your request not succesfully submit');
+            return redirect()->back()->with('success', 'Thank You! Your request succesfully submitted');
+        } else {
+            return redirect()->back()->with('error', 'Your request not succesfully submit');
         }
 
     }
