2ced16ca9d5f48db2e46a4a05f3e2fe12182d15a
developerhardik27 <developerhardik27@gmail.com>
2025-12-30 18:22:35 +0530

    Add DatabaseSedder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,15 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\masterSeeders\StateTableSeeder;
+use Database\Seeders\masterSeeders\CityTableSeeder;
+use Database\Seeders\masterSeeders\UsersTableSeeder;
+use Database\Seeders\masterSeeders\CountryTableSeeder;
+use Database\Seeders\masterSeeders\CurrencyTableSeeder;
+use Database\Seeders\masterSeeders\CompanyTableSeeder;
+use Database\Seeders\masterSeeders\User_roleTableSeeder;
+use Database\Seeders\masterSeeders\User_permissionsTableSeeder;
+use Database\Seeders\masterSeeders\Company_detailsTableSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +27,16 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        $this->call([
+        UsersTableSeeder::class,
+        User_roleTableSeeder::class,
+        User_permissionsTableSeeder::class,
+        StateTableSeeder::class,
+        CurrencyTableSeeder::class,
+        CountryTableSeeder::class,
+        CompanyTableSeeder::class,
+        Company_detailsTableSeeder::class,
+        CityTableSeeder::class,
+        ]);
     }
 }
